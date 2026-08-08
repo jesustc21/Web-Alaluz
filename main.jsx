d83edf5b2f55bf18d3ddff25183ae667335646b1
jesustc21 <jesustc21@gmail.com>
2026-08-08 07:01:02 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import AlaluzReservas from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <AlaluzReservas />
+  </StrictMode>
+);
