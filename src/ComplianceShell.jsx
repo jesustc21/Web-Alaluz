487642ed094e68a5aff05c7161237dcc08daccc6
jesustc21 <jesustc21@gmail.com>
2026-08-14 10:54:33 +0200

    Aislar la fianza para diagnosticar bloqueo del calendario

--- src/ComplianceShell.jsx
+++ src/ComplianceShell.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import AlaluzReservas from "./App.jsx";
-import DepositPricingBridge from "./DepositPricingBridge.jsx";
 import MapSection from "./MapSection.jsx";
 import { CookieConsent, LegalFooter, LegalModal, LEGAL_CSS } from "./LegalCompliance.jsx";
 import { LEGAL_VERSION } from "./legal.js";
@@ -49,7 +48,6 @@ export default function ComplianceShell() {
     <>
       <style>{LEGAL_CSS}</style>
       <AlaluzReservas />
-      <DepositPricingBridge />
       <MapPortal />
       <LegalFooter onOpenLegal={setLegalPage} />
       <CookieConsent onOpenLegal={setLegalPage} />
